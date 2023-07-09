228298b571007b1bbb00cac0a04d6b06fbe4e9fb
DavidRieser <David.Rieser1@innio.com>
2023-07-09 13:32:41 +0200

    Added Option and Result Types

---
+++ Models/Option.cs
@@ -0,0 +1,143 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace ModelManagerServer.Models
+{
+    public readonly struct Option<T>
+    {
+        [MemberNotNullWhen(true, nameof(Value))]
+        // TODO: How does this work if Value is a struct?
+        public bool IsSome { get => this.Value is not null; }
+        public bool IsNone { get => !this.IsSome; }
+
+        private readonly T? Value;
+
+        private Option(T value) { 
+            this.Value = value; 
+        }
+
+        public static Option<T> Some(T value) => new(value);
+        public static Option<T> None => new(default!);
+
+        public static implicit operator Option<T>(T value) => new(value);
+        public static explicit operator T(Option<T> option)
+        {
+            if (!option.IsSome) 
+                throw new ArgumentException("Tried getting Value from Option None Variant!");
+            return option.Value;
+        }
+
+        public Option<T> Filter(Func<T, bool> predicate)
+        {
+            if (this.IsSome && !predicate(this.Value)) 
+                return Option<T>.None;
+            return this;
+        }
+
+        public T Get()
+        {
+            if (!this.IsSome)
+                throw new ArgumentException("Tried getting Value from Option None Variant!");
+            return this.Value;
+        }
+
+        public T GetOr(T _default)
+        {
+            return this.IsSome ? this.Value : _default;
+        }
+
+        public Option<T> Map(Func<T, T> mapper)
+        {
+            return this.IsSome ? Option<T>.Some(mapper(this.Value)) : Option<T>.None;
+        }
+
+        public T MapOr(Func<T, T> mapper, T _default)
+        {
+            return this.IsSome ? mapper(this.Value) : _default;
+        }
+
+        public T MapOrElse(Func<T, T> mapper, Func<T> _default)
+        {
+            return this.IsSome ? mapper(this.Value) : _default();
+        }
+
+        public Option<(T, T1)> Zip<T1>(Option<T1> other)
+        {
+            if (this.IsSome && other.IsSome) 
+                return Option<(T, T1)>.Some((this.Value, other.Value));
+            return Option<(T, T1)>.None;
+        }
+
+        public Option<O> ZipWith<T2, O>(Option<T2> other, Func<T, T2, O> zipper)
+        {
+            var value = !(this.IsSome && other.IsSome) ? zipper(this.Value!, other.Value!) : default!;
+            return new Option<O>(value);
+        }
+
+        // override object.Equals
+        public override bool Equals(object? obj)
+        {
+            if (obj is null) return false;
+            if (obj is not Option<T> opt) return false;
+            return this == opt;
+        }
+
+        // override object.GetHashCode
+        public override int GetHashCode()
+        {
+            if (this.IsSome) return this.Value.GetHashCode();
+            return base.GetHashCode();
+        }
+
+        public static bool operator ==(Option<T> left, Option<T> right)
+        {
+            if (!(left.IsSome && right.IsSome)) return false;
+            return EqualityComparer<T>.Default.Equals(left.Value, right.Value);
+        }
+
+        public static bool operator !=(Option<T> left, Option<T> right)
+        {
+            return !(left == right);
+        }
+    }
+
+    public static class OptionExtensions
+    {
+        public static Option<T> Flatten<T>(this Option<Option<T>> option) {
+            if (option.IsSome) return option.Get();
+            return Option<T>.None;
+        }
+
+        public static (Option<T1>, Option<T2>) Unzip<T1, T2>(this Option<(T1, T2)> option)
+        {
+            if (option.IsSome)
+            {
+                var value = option.Get();
+                return (Option<T1>.Some(value.Item1), Option<T2>.Some(value.Item2));
+            }
+            else
+            {
+                return (Option<T1>.None, Option<T2>.None);
+            }
+        }
+
+        public static Result<T, E> OkOr<T, E>(this Option<T> option, E _default)
+        {
+            return option.IsSome ? Result<T, E>.Ok(option.Get()) : Result<T, E>.Error(_default);
+        }
+
+        public static Result<T, E> OkOrElse<T, E>(this Option<T> option, Func<E> _default)
+        {
+            return option.IsSome ? Result<T, E>.Ok(option.Get()) : Result<T, E>.Error(_default());
+        }
+
+        public static Result<T, E> ErrOr<T, E>(this Option<E> option, T _default)
+        {
+            return option.IsSome ? Result<T, E>.Error(option.Get()) : Result<T, E>.Ok(_default);
+        }
+
+        public static Result<T, E> ErrOrElse<T, E>(this Option<T> option, Func<T> _default)
+        {
+            return option.IsSome ? Result<T, E>.Ok(option.Get()) : Result<T, E>.Ok(_default());
+        }
+    }
+}
---
+++ Models/Result.cs
@@ -0,0 +1,135 @@
+﻿namespace ModelManagerServer.Models
+{
+    public readonly struct Result<T, E>
+    {
+        public bool IsOk { get; }
+        public bool IsError { get => !this.IsOk; }
+        private readonly object Value;
+
+        private Result(bool isOk, object value)
+        {
+            this.IsOk = isOk;
+            this.Value = value;
+        }
+
+        public static Result<T, E> Ok(T value)
+        {
+            return new Result<T, E>(true, value!);
+        }
+        public static Result<T, E> Error(E value)
+        {
+            return new Result<T, E>(false, value!);
+        }
+
+        public static implicit operator Result<T, E>(T value)
+        {
+            return Ok(value);
+        }
+
+        public static implicit operator Result<T, E>(E error)
+        {
+            return Error(error);
+        }
+
+        public static explicit operator T(Result<T, E> result)
+        {
+            return result.Get();
+        }
+
+        public static explicit operator E(Result<T, E> result)
+        {
+            return result.TakeError();
+        }
+
+        public Result<TNew, E> Map<TNew>(Func<T, TNew> func)
+        {
+            return new Result<TNew, E>(this.IsOk, this.IsOk ? func((T)this.Value)! : this.Value);
+        }
+
+        public Result<T, ENew> MapError<ENew>(Func<E, ENew> func)
+        {
+            return new Result<T, ENew>(this.IsOk, this.IsError ? func((E)this.Value)! : this.Value);
+        }
+
+        public Result<T, ENew> MapOr<ENew>(T _default)
+        {
+            return this.IsOk ? (T)this.Value : _default;
+        }
+
+        public Result<T, ENew> MapOrElse<ENew>(Func<E, T> _default)
+        {
+            return this.IsOk ? (T)this.Value : _default((E)this.Value);
+        }
+
+        public T Get()
+        {
+            if (!this.IsOk)
+                throw new ArgumentException("Tried getting Ok Value from Error Result!");
+            return (T)this.Value;
+        }
+
+        public T GetOr(T _default)
+        {
+            return this.IsOk ? (T)this.Value : _default;
+        }
+
+        public T GetOrElse(Func<E, T> _default)
+        {
+            return this.IsOk ? (T)this.Value : _default((E)this.Value);
+        }
+
+        public T GetOrDefault()
+        {
+            return this.IsOk ? (T)this.Value : default!;
+        }
+
+        public E TakeError()
+        {
+            if (this.IsOk)
+                throw new ArgumentException("Tried getting Error Value from Ok Result!");
+            return (E)this.Value;
+        }
+
+        // override object.Equals
+        public override bool Equals(object? obj)
+        {
+            if (obj is null) return false;
+            if (obj is not Result<T, E> res) return false;
+            return this == res;
+        }
+
+        // override object.GetHashCode
+        public override int GetHashCode()
+        {
+            return this.Value.GetHashCode();
+        }
+
+        public static bool operator ==(Result<T, E> left, Result<T, E> right)
+        {
+            if (left.IsOk != right.IsOk) return false;
+            return left.Value == right.Value;
+        }
+
+        public static bool operator !=(Result<T, E> left, Result<T, E> right)
+        {
+            return !(left == right);
+        }
+    }
+
+    public static class ResultExtensions {
+        public static Result<T, E> Flatten<T, E>(this Result<Result<T, E>, E> result)
+        {
+            return result.IsOk ? result.Get() : Result<T, E>.Error(result.TakeError());
+        }
+
+        public static Option<T> Ok<T, E>(this Result<T, E> result) 
+        {
+            return result.IsOk ? Option<T>.Some(result.Get()) : Option<T>.None;
+        }
+
+        public static Option<E> Err<T, E>(this Result<T, E> result)
+        {
+            return !result.IsOk ? Option<E>.Some(result.TakeError()) : Option<E>.None;
+        }
+    }
+}
