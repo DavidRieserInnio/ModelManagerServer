1ad9fa89da51afdf77a585c70d70a5db39cd9789
DavidRieser <David.Rieser1@innio.com>
2023-07-12 18:06:14 +0200

    Added some Extension Methods

---
+++ Service/Extensions.cs
@@ -0,0 +1,16 @@
+﻿namespace ModelManagerServer.Service
+{
+    public static class Extensions
+    {
+        public static Dictionary<K, V> ToDictionary<K, V>(this ICollection<KeyValuePair<K, V>> collection)
+            where K : notnull
+        {
+            return collection.ToDictionary(kvp => kvp.Key, kvp => kvp.Value);
+        }
+
+        public static IEnumerable<T>? CheckEmpty<T>(this IEnumerable<T> values)
+        {
+            return values.Any() ? values : null;
+        }
+    }
+}
