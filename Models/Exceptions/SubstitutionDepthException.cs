7b0a20f720472c6a07190a878998b51e2001d6f3
DavidRieser <David.Rieser1@innio.com>
2023-07-20 01:39:16 +0200

    Added Substitution Function for Template Values

---
+++ Models/Exceptions/SubstitutionDepthException.cs
@@ -0,0 +1,19 @@
+﻿using System.Runtime.Serialization;
+
+namespace ModelManagerServer.Models.Exceptions
+{
+    [Serializable]
+    public class SubstitutionDepthException : SubstitutionException
+    {
+        public SubstitutionDepthException(string expression, uint depth)
+            : base($"Reached Maximum Substitution Depth {depth} for Expression {{{expression}}}!") { }
+
+        protected SubstitutionDepthException(SerializationInfo info, StreamingContext context)
+            : base(info, context) { }
+
+        public override void GetObjectData(SerializationInfo info, StreamingContext context)
+        {
+            base.GetObjectData(info, context);
+        }
+    }
+}
--- Service/ConversionService.cs
+++ Service/ConversionService.cs
@@ -1,14 +1,71 @@
 ﻿using ModelManagerServer.Entities;
+using ModelManagerServer.Models;
+using ModelManagerServer.Models.Exceptions;
+using ModelManagerServer.Models.Interfaces;
 
 namespace ModelManagerServer.Service
 {
     public static class ConversionService
     {
-        public static List<Part> ConvertModel(Model model, Func<string, string?> lookup)
+        public const uint MAXIMUM_SUBSTITUTION_DEPTH = 50;
+
+        public static List<Part> ConvertModel(
+            Model model,
+            ISubstitutionProvider userDefinedSubstitutions
+        )
         {
+            var tmpvls = model.TemplateValues;
+            var provider = tmpvls.ToSubstitutionProvider(userDefinedSubstitutions);
 
 
             return new();
+        }
+
+        public static ISubstitutionProvider ToSubstitutionProvider(
+            this List<TemplateValue> templateValues,
+            ISubstitutionProvider additionalSubstitutions,
+            uint max_substitution_depth = MAXIMUM_SUBSTITUTION_DEPTH
+        )
+        {
+            var dict = templateValues.ToDictionary(t => t.Name, t => t.Value);
+            DictionarySubstitutionProvider tv_provider = dict;
+            var sub_provider = new SubstitutionProviderCollection(tv_provider, additionalSubstitutions);
+            Func<string, string> lookup = s => sub_provider.GetSubstitution(s).GetOr(null!);
+
+            foreach (var tv in templateValues)
+            {
+                uint i;
+                var (key, value) = (tv.Name, tv.Value);
+
+                for (i = 0; i < max_substitution_depth; i++)
+                {
+                    var rep = StringService.ReplaceOccurrences(value, lookup);
+                    if (rep.IsOk)
+                    {
+                        var newValue = rep.Get();
+                        if (newValue is not null)
+                        {
+                            value = newValue;
+                            dict[key] = newValue;
+                        }
+                        else break;
+                    } 
+                    else
+                    {
+                        // TODO: Return Result?
+                        throw rep.GetError();
+                    }
+                }
+
+                if (i >= max_substitution_depth)
+                {
+                    // TODO: This Error Message prints the Value with already substituted Values
+                    //       but should print the original one without any substitutions.
+                    throw new SubstitutionDepthException(value, max_substitution_depth);
+                }
+            }
+
+            return null!;
         }
     }
 }
