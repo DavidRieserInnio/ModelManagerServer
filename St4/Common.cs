a04b7233423140fe610c717d0c77d3817d58885d
DavidRieser <David.Rieser1@innio.com>
2023-07-26 23:34:41 +0200

    Updated Common.cs from MyProject

--- St4/Common.cs
+++ St4/Common.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections.Generic;
-using System.Drawing;
+﻿using System.Text.RegularExpressions;
 
 namespace ModelManagerServer.St4
 {
@@ -8,40 +7,15 @@ namespace ModelManagerServer.St4
     /// </summary>
     public static class Common
     {
-        public const bool RightFactoryEnable = true;
-
-        /// <summary>the value displayed for an empty ComboBox</summary>
-        /// <value>--</value>
-        public const string ComboEmptyValue = "--";
-
-        public const string TextBoxMaxLengthDefaultValue = "1000";
-
-        public const string FallbackLanguage = "de";
-
+        public static bool DevMode { get; internal set; } = true;
+        public static Guid AppId_Configurator { get; } = Guid.Parse("cc96031c-8e2e-49fd-af1f-c734f55948fe");
+        public static string FallbackLanguage { get; } = "de";
+        public static string ComboEmptyValue { get; } = "--";
+        public static string TextBoxMaxLengthDefaultValue { get; } = "1000";
         public static bool CheckForSpecialPartPreliminary { get; } = false;
         public static bool CheckForSpecialPartSalesOrder { get; } = false;
+        public static Regex RegexDeviceNumber { get; } = new Regex(@"[A-Z0-9]{4}", RegexOptions.Compiled, TimeSpan.FromSeconds(1));
 
-        #region File System
-
-        /// <summary>Filename of the usersettings file in temp folder</summary>
-        public const string UserSettingsFolderName = "ST4_Konfigurator";
-
-        public const string UserSettingsFileName = "UserSettings.xml";
-        public static string AppSettingsFileName { get; } = "AppSettings.xml";
-        public const string ConnectionStringFileName = "Configurator.Database.ConnString.bin";
-
-        #endregion
-
-        #region Colors
-
-        // infobox und notifybox header
-        public static readonly Color ViewletHeaderBackcolorSelected = Color.FromArgb(0, 102, 182);
-        public static readonly Color ViewletHeaderBackcolorUnSelected = Color.FromArgb(240, 240, 240);
-
-        public static readonly Color FilterActiveColor = Color.Bisque;
-        public static readonly Color InputControlBackcolor = Color.White; //Color.FromArgb(240, 240, 240);
-
-        #endregion
 
         #region DataSources
 
@@ -56,6 +30,7 @@ namespace ModelManagerServer.St4
         public static string DataSourceNoInput { get; } = "DS_09";
         public static string DataSourceOldConfigurator { get; } = "DS_10";
         public static string DataSourceConfigurationTemplate { get; } = "DS_11";
+        public static string DataSourceDefaultValue { get; } = "DS_12";
 
         #endregion
 
@@ -67,6 +42,14 @@ namespace ModelManagerServer.St4
 
         #endregion
 
+        #region Milestones
+
+        public static string MilestoneBase { get; } = "MS_01";
+        public static string MilestoneUpcoming { get; } = "MS_02";
+        public static string MilestoneReached { get; } = "MS_03";
+
+        #endregion
+
         #region Parts Special Usage Property
 
         public static string PartSpecialUsageDeviceNumber { get; } = "JNr";
@@ -75,35 +58,48 @@ namespace ModelManagerServer.St4
         public static string PartSpecialUsagePreliminary { get; } = "Preliminary";
         public static string PartSpecialUsageEngineCount { get; } = "EngineCount";
         public static string PartSpecialUsageLanguage { get; } = "Language";
+        public static string PartSpecialUsageDevice { get; } = "Device";
+        public static string PartSpecialUsageCountry { get; } = "Country";
+
+        #endregion
+
+        #region Parts Names
+
+        public static string PartNameDeviceNumber { get; } = "FD.DeviceNumber";
 
         #endregion
 
         #region Properties Keys
 
-        public const string PropertyVisibleRule = "VisibleRule";
-        public const string PropertyCircuit = "Circuit";
-        public const string PropertyGroup = "Group";
-        public const string PropertyName = "Name";
-        public const string PropertyWatermark = "Watermark";
-        public const string PropertyUnit = "Unit";
-        public const string PropertyArticleCode = "ArticleCode";
-        public const string PropertyComboItemText = "ItemText";
+        public static string PropertyVisibleRule { get; } = "VisibleRule";
+        public static string PropertyCircuit { get; } = "Circuit";
+        public static string PropertyGroup { get; } = "Group";
+        public static string PropertyName { get; } = "Name";
+        public static string PropertyWatermark { get; } = "Watermark";
+        public static string PropertyUnit { get; } = "Unit";
+        public static string PropertyArticleCode { get; } = "ArticleCode";
+        public static string PropertyComboItemText { get; } = "ItemText";
+        public static string PropertyComboItemSortMode { get; } = "ComboItemSortMode";
 
-        public const string PropertyMandatory = "Mandatory";
-        public const string PropertySpecialFlag = "Special";
-        public const string PropertyRegex = "Regex";
-        public const string PropertyMaxLength = "MaxLength";
-        public const string PropertyElementText = "ElementText";
-        public const string PropertyInfoDocument = "DOC";
-        public const string PropertyInfoPlcLabel = "SPS";
-        public const string PropertyInfoParameter = "PAR";
-        public const string PropertyInfoPid = "P&ID";
-        public const string PropertyInfoSsl = "SSL";
-        public const string PropertyInfoOracleKey = "Oracle Key";
-        public const string PropertyInfoOracleValue = "Oracle Value";
-        public const string PropertyInfo = "INF";
-        public const string PropertyMilestoneId = "Milestone Id";
-        public const string PropertyModelInstanceId = "Model Instance Id";
+        public static string PropertyMandatory { get; } = "Mandatory";
+        public static string PropertySpecialFlag { get; } = "Special";
+        public static string PropertyRegex { get; } = "Regex";
+        public static string PropertyMaxLength { get; } = "MaxLength";
+        public static string PropertyElementText { get; } = "ElementText";
+        public static string PropertyInfoDocument { get; } = "DOC";
+        public static string PropertyInfoPlcLabel { get; } = "SPS";
+        public static string PropertyInfoParameter { get; } = "PAR";
+        public static string PropertyInfoPid { get; } = "P&ID";
+        public static string PropertyInfoSsl { get; } = "SSL";
+        public static string PropertyInfoOracleKey { get; } = "Oracle Key";
+        public static string PropertyInfoOracleValue { get; } = "Oracle Value";
+        public static string PropertyInfoInternal { get; } = "INF";
+        public static string PropertyInfoExternal { get; } = "Info Partner";
+        public static string PropertyMilestoneId { get; } = "Milestone Id";
+        public static string PropertyOverhaulAndRepairType { get; } = "Overhaul And Repair";
+        public static string PropertySeparatorBefore { get; } = "Separator Before";
+        public static string PropertyTextboxType { get; } = "InputType";
+        public static string PropertyDefaultValue { get; } = "DefaultValue";
 
         public static List<string> TranslatablePropertyKeys { get; } = new List<string>
         {
@@ -111,7 +107,8 @@ namespace ModelManagerServer.St4
             PropertyGroup,
             PropertyCircuit,
             PropertyWatermark,
-            PropertyInfo,
+            PropertyInfoInternal,
+            PropertyInfoExternal,
             PropertyComboItemText
         };
 
@@ -123,6 +120,9 @@ namespace ModelManagerServer.St4
         public static string DocumentChecklist { get; } = "CL";
         public static string DocumentTechnicalSpecification { get; } = "TSOC";
         public static string DocumentInterfaceList { get; } = "SSL";
+        public static string DocumentWiringDiagram { get; } = "WD";
+        public static string DocumentSoftware { get; } = "SW";
+        public static string DocumentParameter { get; } = "Para";
 
         #endregion
 
@@ -137,18 +137,21 @@ namespace ModelManagerServer.St4
 
         #endregion
 
-        #region Software Function Rights
+        #region EAI
 
-        public static string DeleteUnreleasedRightName { get; } = "DeleteUnreleasedProject";
-        public static string DeleteReleasedRightName { get; } = "DeleteReleasedProject";
-        public static string IgnoreMaintenanceMode { get; } = "IgnoreMaintenanceMode";
+        public static string EaiDirectory { get; } = @"\\jenwap040.ad.innio.com\Shared\Schema_ST4\EAI_Connector\";
+        public static string EaiCreationFileNamespace { get; } = @"http://www.schema.de/2012/ST4/APX/ControlFile";
+        public static string St4TpeTemplatesFolder { get; } = @"/SystemFolder/SystemFolder[@Title = 'Content']/*[@Title='Default']/*[@Title='Vorlagen']/*[@Title='ETechnik']/*[@Title='EAI Connector']/*";
+        public static string St4TpeProductionLayout { get; } = @"string:/Production/GE Jenbacher - TPE_INNIO/";
+        public static string St4TpeProductionLayout_myPlant { get; } = @"string:/Production/Export Layouts/";
+        public static string St4TpeProductionLayout_ClTestbench { get; } = @"QML to Qdas";
+        public static string St4TpeProductionLayout_ClService { get; } = @"Export Checkliste myPlant [multilanguage]";
+        public static string myPlantExportPath { get; } = @"string:\\jenwap040.ad.innio.com\Shared\checklist_transfer\open\";
+        public static string TitleBackPageStPath { get; } = @".//InfoType15/TextModule2[@Title = 'Änderungen [->Sprachneutral]']/@Content/@All";
 
-        public static string ReleaseProjectObsoleteConfigVersionRightName { get; } = "ReleaseProjectObsoleteConfigVersionRightName";
-        public static string ExportFilesObsoleteConfigVersionRightName { get; } = "ExportFilesObsoleteConfigVersionRightName";
-        public static string ExportUnreleasedProjectRightName { get; } = "ExportUnreleasedProjectRightName";
-
-        public static string ValidationModeRightName { get; } = "ValidationModeRightName";
-
+        public static string EaiFilterFileName { get; } = "TPE_Filter.csv";
+        public static string EaiCsvFileName { get; } = "TPE_ProjectVariables.csv";
         #endregion
+
     }
 }
